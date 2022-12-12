e72598dadf566eda37dcd1a0749bbf08825d9494
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2022-12-12 23:57:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../styles/App.css";
 import User from "../models/user";
 
 const App = () => {
+  const [isSignedUp, setIsSignedUp] = useState(false);
+  const [isLoggenIn, setIsLoggedIn] = useState(false);
+  const [user,setUser] = useState({name:'',email:'',password:''});
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passRef = useRef();
+  const confPassRef = useRef();
+  const loginEmailRef = useRef();
+  const loginPassRef = useRef();
+  const signUpHandler = (e)=> {
+    // e.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const pwd = passRef.current.value;
+    const cpwd = confPassRef.current.value;
+    console.log(pwd,cpwd);
+    if(!name || !email || !pwd || !cpwd) return;
+    if(pwd === cpwd) {
+      setUser({name,email,password:pwd})
+      setIsSignedUp(true);
+      // console.log('yes');
+    }
+  }
+  const handleLogin = ()=> {
+    const enteredEmail = loginEmailRef.current.value;
+    const enteredPassword = loginPassRef.current.value;
+    if(isSignedUp && enteredEmail === user.email && enteredPassword == user.password){
+      setIsLoggedIn(true);
+    }
+  }
+
+  const handleLogout = ()=> {
+    setIsLoggedIn(false);
+  }
   return (
     <div id="main">
       <table id="all-users">
@@ -13,43 +47,44 @@ const App = () => {
             <th>Password</th>
           </tr>
           <tr>
-            <td>{/** user's name */}</td>
-            <td>{/** user's email */}</td>
-            <td>{/** user's password */}</td>
+            <td>{isSignedUp && user.name}</td>
+            <td>{isSignedUp && user.email}</td>
+            <td>{isSignedUp && user.password}</td>
           </tr>
         </tbody>
       </table>
 
       <div>
-        <form className="signup-form">
+        <form className="signup-form" onSubmit={(e)=>{e.preventDefault()}}>
           <label htmlFor="name">Name</label>
-          <input type="text" name="signupName" id="signupName" />
+          <input type="text" name="signupName" id="signupName" ref={nameRef}/>
           <label htmlFor="email">Email</label>
-          <input type="email" name="signupEmail" id="signupEmail" />
+          <input type="email" name="signupEmail" id="signupEmail" ref={emailRef}/>
           <label htmlFor="password">Password</label>
-          <input type="password" name="signupPassword" id="signupPassword" />
+          <input type="password" name="signupPassword" id="signupPassword" ref={passRef}/>
           <label htmlFor="confirmPassword">Confirm Password</label>
           <input
             type="password"
             name="signupConfirmPassword"
             id="signupConfirmPassword"
+            ref={confPassRef}
           />
         </form>
-        <button id="signup-button">Signup</button>
-        <form className="login-styles">
+        <button id="signup-button" onClick={signUpHandler}>Signup</button>
+        {!isLoggenIn && (<form className="login-styles" onClick={(e)=>{e.preventDefault()}}>
           <label htmlFor="loginEmail">Email</label>
-          <input id="loginEmail" name="loginEmail" type="email" />
+          <input id="loginEmail" name="loginEmail" type="email" ref={loginEmailRef}/>
           <label htmlFor="loginPassword">Password</label>
-          <input id="loginPassword" name="loginPassword" type="password" />
-        </form>
-        <button id="login-button">Login</button>
+          <input id="loginPassword" name="loginPassword" type="password" ref={loginPassRef}/>
+        </form>)}
+        {!isLoggenIn && (<button id="login-button" onClick={handleLogin}>Login</button>)}
       </div>
 
-      <div>
-        <h3 id="username">{/** Logged in user's name */}</h3>
-        <h3 id="email">{/** Logged in user's email */}</h3>
-        <button id="logout-button">Logout</button>
-      </div>
+      {isLoggenIn && (<div>
+        <h3 id="username">{user.name}</h3>
+        <h3 id="email">{user.email}</h3>
+        <button id="logout-button" onClick={handleLogout}>Logout</button>
+      </div>)}
     </div>
   );
 };
